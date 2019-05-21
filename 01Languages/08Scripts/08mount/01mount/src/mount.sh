74f9721abef194d0dd9b92c2b252f279be1b3a20
vdedauw <46540943+vdedauw@users.noreply.github.com>
2019-05-21 13:44:12 +0200

    Add files via upload

--- 01Languages/08Scripts/08mount/01mount/src/mount.sh
+++ 01Languages/08Scripts/08mount/01mount/src/mount.sh
@@ -33,71 +33,163 @@
 # 0.1.2 added setValues
 # 0.1.3 removed setValues, added commands array
 # 0.1.4 added usage
+# 0.1.5 added checks individual message
+# 0.1.6 define arrays and parameter values close to each other, improves readability
+# 0.1.7 for checks: use regular array, rather then associative arrays
 
 SCRIPT="mount.sh"
-VERSION="0.1.3"
+VERSION="0.1.7"
 DATE="2019/05/15"
 MES=" "
 
-# return value
-# when 0 the parameter is not checked if set
+# return value, default value 0
 RETVAL=0
-# command line parameter not set return values
-NO_TAG=1
-NO_COMMAND=2
-NO_HOST=3
-NO_EXPORT=4
-NO_DIRECTORY=5
-NO_MOUNT=6
-NO_PORT=7
-NO_SIZE=8
-NO_ROOT=9
+
+declare -A SYNTAXS
+declare -A VALUES
+declare -A RETVALS
+declare -A HASPARAM
+declare -A CHECK1
+declare -A ERROR1
+declare -A CHECK2
+declare -A ERROR2
+declare -A CHECK3
+declare -A ERROR3
+declare -A CHECK4
+declare -A ERROR4
+declare -A CHECK5
+declare -A ERROR5
 
 # return values indicating incorrect command line values are the same as the check numbers
 # as a documentation feature they are repeated as an error name
 TNAME="tag"
 IS_TAG=1                 # dummy value, no check for 'tag'
+NO_TAG=1
+ SYNTAXS+=([$TNAME]="tag=")
+ RETVALS+=([$TNAME]=$NO_TAG)
+HASPARAM+=([$TNAME]="N")
 
+# the command for this script
+CNAME="command"
+NO_COMMAND=10
+
+# local domain definitions
 DOMAIN="domain"
-IS_DNS_DOMAIN=2          # checks if domain is a local dns domain
+NO_DOMAIN=10
+LOCAL_DNS_DOMAIN=11    # checks if domain is a local dns domain
+NO_LOCAL_DOMAIN=11
+ SYNTAXS+=([$DOMAIN]="domain=")
+ RETVALS+=([$DOMAIN]=$NO_DOMAIN)
+HASPARAM+=([$DOMAIN]="N")
 
+# host definitions
 HOST="nfshost"
-HOST_ONLINE=10           # checks if the host is online
-NOT_ONLINE=10
-HOST_IN_DNS=11           # checks if the host in in the (local) dns database
-UNKNOWN_HOST=11
+NO_HOST=20
+# checks if the host is online
+HOST_ONLINE_TITLE="is host online ?"
+HOST_ONLINE_MESSAGE="host not online !"
+HOST_ONLINE_ERROR=21
+# checks if the host in in the (local) dns database
+HOST_DOMAIN="host in local domain ?"
+HOST_DOMAIN_MESSAGE="host not in local domain !"
+HOST_DOMAIN_ERROR=22
 
+ SYNTAXS+=([$HOST]="nfshost=")
+ RETVALS+=([$HOST]=$NO_HOST)
+HASPARAM+=([$HOST]="Y")
+  CHECK1+=([$HOST_ONLINE_TITLE]=$HOST_ONLINE_MESSAGE)
+  ERROR1+=([$HOST_ONLINE_TITLE]=$HOST_ONLINE_ERROR)
+
+# export definitions
 EXPORT="nfsexport"
-IS_EXPORT=20             # checks if this value is an remote export
-NOT_EXPORTED=20
-IS_EXPORTED=21           # checks if this value is an exported directory
-NOT_EXPORTED=21
+NO_EXPORT=30
+# checks if this value is a remote export
+IS_EXPORT_TITLE="is remote export ?"
+IS_EXPORT_MESSAGE="is not a remote export !"
+IS_EXPORT_ERROR=31
+# checks if this value is an exported directory
+IS_EXPORTED_TITLE="is exported ?"
+IS_EXPORTED_MESSAGE="is not exported !"
+IS_EXPORTED_ERROR=32
 
-# this is also 'nfsmount', mount is a directory
-IS_DIRECTORY=30          # checks if this value is an existing directory
-DIR_NOT_EXIST=30
-NEW_DIRECTORY=31         # creates this directory
-DIR_CANNOT_CREATE=31
+ SYNTAXS+=([$EXPORT]="nfsexport=")
+ RETVALS+=([$EXPORT]=$NO_EXPORT)
+HASPARAM+=([$EXPORT]="Y")
+ CHECK1+=([$IS_EXPORT_TITLE]=$IS_EXPORT_MESSAGE)
+ ERROR1+=([$IS_EXPORT_TITLE]=$IS_EXPORT_ERROR)
 
 MOUNT="nfsmount"
-IS_MOUNTED=40            # checks if this value is mounted
-ALREADY_MOUNTED=40
-IS_EXPORTED=41           # checks if this value is exported
-NOT_EXPORTED=41
+NO_MOUNT=40
+# checks if this value is a directory
+IS_DIRECTORY_TITLE="is directory ?"
+IS_DIRECTORY_MESSAGE="is not directory !"
+IS_DIRECTORY_ERROR=41
+# creates this directory
+NEW_DIRECTORY_TITLE="create directory."
+NEW_DIRECTORY_MESSAGE="could not create directory !"
+NEW_DIRECTORY_ERROR=42
+# checks if this value is already mounted
+ALREADY_MOUNTED_TITLE="already mounted ?"
+ALREADY_MOUNTED_MESSAGE="already mounted ! Use mount.sh unmount first."
+ALREADY_MOUNTED_ERROR=43
+# checks if this value is mounted
+IS_MOUNTED_TITLE="is mounted ?"
+IS_MOUNTED_MESSAGE="not mounted !"
+IS_MOUNTED_ERROR=44
+
+ SYNTAXS+=([$MOUNT]="nfsmount=")
+ RETVALS+=([$MOUNT]=$NO_MOUNT)
+HASPARAM+=([$MOUNT]="Y")
+  CHECK1+=([$IS_DIRECTORY_TITLE]=$IS_DIRECTORY_MESSAGE)
+  ERROR1+=([$IS_DIRECTORY_TITLE]=$IS_DIRECTORY_ERROR)
+  CHECK2+=([$NEW_DIRECTORY_TITLE]=$NEW_DIRECTORY_MESSAGE)
+  ERROR2+=([$NEW_DIRECTORY_TITLE]=$NEW_DIRECTORY_ERROR)
+  CHECK3+=([$ALREADY_MOUNTED_TITLE]=$ALREADY_MOUNTED_MESSAGE)
+  ERROR3+=([$ALREADY_MOUNTED_TITLE]=$ALREADY_MOUNTED_ERROR)
+  CHECK4+=([$IS_MOUNTED_TITLE]=$IS_MOUNTED_MESSAGE)
+  ERROR4+=([$IS_MOUNTED_TITLE]=$IS_MOUNTED_ERROR)
 
 PORT="nfsport"
-HAS_PORT=50              # checks if the remote host has a port
-NO_REMOTE_PORT=50
+NO_PORT=50
+# checks if the remote host has a port
+HAS_PORT_TITLE="has remote port ?"
+HAS_PORT_MESSAGE="host has no remote port !"
+HAS_PORT_ERROR=51
 
-IS_SERVICE=51            # checks if the local host has a service
-NO_SERVICE=51
+ SYNTAXS+=([$PORT]="nfsport=")
+ RETVALS+=([$PORT]=$NO_PORT)
+HASPARAM+=([$PORT]="Y")
+  CHECK1+=([$HAS_PORT_TITLE]=$HAS_PORT_MESSAGE)
+  ERROR1+=([$HAS_PORT_TITLE]=$HAS_PORT_ERROR)
+
+SERVICE="nfsservice"
+NO_SERVICE=60
+# checks if the local host has a service
+HAS_SERVICE_TITLE="has service ?"
+HAS_SERVICE_MESSAGE="has no service !"
+HAS_SERVICE_ERROR=61
 
 SIZE="nfssize"
-IS_SIZE=60               # checks if the volume has the expected size
-INCORRECT_SIZE=60
+NO_SIZE=70
+# checks if the volume has the expected size
+IS_SIZE_TITLE="is size ?"
+IS_SIZE_MESSAGE="warning sizes not equal !"
+IS_SIZE_ERROR=71
+
+ SYNTAXS+=([$SIZE]="nfssize=")
+ RETVALS+=([$SIZE]=$NO_SIZE)
+HASPARAM+=([$SIZE]="N")
+  CHECK5+=([$IS_SIZE_TITLE]=$IS_SIZE_MESSAGE)
+  ERROR5+=([$IS_SIZE_TITLE]=$IS_SIZE_ERROR)
 
 ROOT="root"
-IS_ROOT=70               # checks if the current user is root
+NO_ROOT=80
+# checks if the current user is root
+IS_ROOT_TITLE="user is root user ?"
+IS_ROOT_MESSAGE="$SCRIPT user needs to be root !"
+IS_ROOT_ERROR=81
+ CHECK1+=([$IS_ROOT_TITLE]=$IS_ROOT_MESSAGE)
+ ERROR1+=([$IS_ROOT_TITLE]=$IS_ROOT_ERROR)
 
 # define local vars
 TAG="MOUNT"
@@ -107,34 +199,6 @@ PROGRAMS=("gawk" "grep" "nmap" "ping" "mount" "showmount" "umount" "df" "whoami"
 PARAMS=("$@")
 OPTIONS=
 COMMANDS=("silence" "mount" "unmount" "show" "free" "exports" "usage")
-
-declare -A SYNTAXS
-SYNTAXS=([$TNAME]="tag=" [$HOST]="nfshost=" [$EXPORT]="nfsexport=" [$MOUNT]="nfsmount=" [$PORT]="nfsport=" [$SIZE]="nfssize=")
-declare -A VALUES
-declare -A RETVALS
-RETVALS=([$TNAME]=$NO_TAG [$HOST]=$NO_HOST [$EXPORT]=$NO_EXPORT [$MOUNT]=$NO_DIRECTORY [$PORT]=$NO_PORT [$SIZE]=$NO_SIZE)
-declare -A HASPARAM
-HASPARAM=([$TNAME]="N" [$HOST]="Y" [$EXPORT]="Y" [$MOUNT]="Y" [$PORT]="Y" [$SIZE]="N")
-
-# before cycle
-declare -A BEFORE_RETVALS
-BEFORE_RETVALS=([$HOST]=$NOT_ONLINE [$EXPORT]=$NOT_EXPORTED [$MOUNT]=$CANNOT_CREATE [$PORT]=$NO_REMOTE_PORT [$SIZE]=$INCORRECT_SIZE [$ROOT]=$NO_ROOT)
-declare -A BEFORE_CHECKS
-BEFORE_CHECKS=([$HOST]=$HOST_ONLINE [$EXPORT]=$IS_EXPORT [$MOUNT]=$IS_DIRECTORY [$PORT]=$HAS_PORT [$SIZE]=$CHECK_SIZE [$ROOT]=$IS_ROOT)
-# middle cycle
-declare -A MIDDLE_RETVALS
-MIDDLE_RETVALS=([$MOUNT]=$ALREADY_MOUNTED )
-declare -A MIDDLE_CHECKS
-MIDDLE_CHECKS=([$MOUNT]=$IS_MOUNTED)
-# final cycle
-declare -A AFTER_RETVALS
-AFTER_RETVALS=([$SIZE]=$INCORRECT_SIZE)
-declare -A AFTER_CHECKS
-AFTER_CHECKS=([$SIZE]=$IS_SIZE)
-declare -A MOUNT_RETVALS
-MOUNT_RETVALS=([$MOUNT]=$NOT_DIRECTORY [$ROOT]=$NO_ROOT)
-declare -A MOUNT_CHECKS
-MOUNT_CHECKS=([$MOUNT]=$IS_DIRECTORY [$ROOT]=$IS_ROOT)
 
 echoIt() {
    if [ -z $SILENCE ]; then SILENCE="---"; fi
@@ -254,62 +318,92 @@ hasCommandLineParameters() {
 }
 
 checkCommandLineParameter() {
-        if [ -z $CHECK ]; then return; fi
+	WC=$(echo $TITLE | wc -w)
+        if [ $WC -eq 0 ]; then return; fi
         # if HASCHECK indicates it, check it
-        MES="      checking $NAME value=$VALUE"
-        echoIt
-        case $CHECK in
-                $HOST_ONLINE) # checks if the host is online
+
+        case $TITLE in
+                $HOST_ONLINE_TITLE) # checks if the host is online
+			MES="      ${VALUES[$HOST]} $TITLE"
+			echoIt
                         ping -c 1 ${VALUES[$HOST]} > /dev/null
                         if [ ! $? -eq 0 ]; then
-                                MES="host ${VALUES[$HOST]} not online"
+                                MES="${VALUES[$HOST]} $MESSAGE"
                                 loggerExit
                         fi
                 ;;
-		$IS_EXPORT)
+		$IS_EXPORT_TITLE)
+			MES="      ${VALUES[$HOST]}:${VALUES[$EXPORT]} $TITLE"
+			echoIt
 			showmount -e ${VALUES[$HOST]} | grep ${VALUES[$EXPORT]} > /dev/null
 			if [ ! $? -eq 0 ]; then
-				MES="host $NFSHOST has not an export named $NFSEXPORT"
+				MES="${VALUES[$HOST]}:${VALUES[$EXPORT]} $MESSAGE"
 				loggerExit
 			fi
  		;;
-                $IS_DIRECTORY)
+                $IS_DIRECTORY_TITLE)
                         # check if directory exists, if not --> could not create it
+			MES="      ${VALUES[$MOUNT]} $TITLE"
+			echoIt
                         if [ ! -d ${VALUES[$MOUNT]} ]; then
-				mkdir ${VALUES[$MOUNT]}
-                        fi
-                        if [ ! -d ${VALUES[$MOUNT]} ]; then
-                                MES="directory ${VALUES[$MOUNT]} cannot be created"
+                                MES="${VALUES[$MOUNT]} $MESSAGE"
 				loggerExit
                         fi
+		;;
+                $NEW_DIRECTORY_TITLE)
+                        # check if directory exists, if not --> could not create it
+                        MES="      ${VALUES[$MOUNT]} $TITLE"
+                        echoIt
+			mkdir ${VALUES[$MOUNT]} > /dev/null 2> /dev/null
+                        if [ ! -d ${VALUES[$MOUNT]} ]; then
+                                MES="${VALUES[$MOUNT]} $MESSAGE"
+                                loggerExit
+                        fi
                 ;;
-		$HAS_PORT)
+                $ALREADY_MOUNTED_TITLE)
+                        MES="      ${VALUES[$MOUNT]} $TITLE"
+                        echoIt
+                        df -h | grep ${VALUES[$MOUNT]} > /dev/null
+                        if [ $? -eq 0 ]; then
+                                MES="${VALUES[$MOUNT]} $MESSAGE"
+                                loggerExit
+                        fi
+                ;;
+                $IS_MOUNTED_TITLE)
+                        MES="      ${VALUES[$MOUNT]} $TITLE"
+                        echoIt
+                        df -h | grep ${VALUES[$MOUNT]} > /dev/null
+                        if [ ! $? -eq 0 ]; then
+                                MES="${VALUES[$MOUNT]} $MESSAGE"
+                                loggerExit
+                        fi
+                ;;
+		$HAS_PORT_TITLE)
+			MES="      ${VALUES[$HOST]}:${VALUES[$PORT]} $TITLE"
+			echoIt
 			nmap ${VALUES[$HOST]} | grep ${VALUES[$PORT]} > /dev/null
                         if [ ! $? -eq 0 ]; then
-                                MES="host ${VALUES[$HOST]} has not port ${VALUES[$PORT]}"
+                                MES="${VALUES[$HOST]}:${VALUES[$PORT]} $MESSAGE"
                                 loggerExit
                         fi
 		;;
-		$IS_SIZE)
+		$IS_SIZE_TITLE)
+			MES="      ${VALUES[$SIZE]} $TITLE"
+			echoIt
 			LINE=$(df -h | grep ${VALUES[$MOUNT]})
 			ISSIZE=$(echo $LINE | gawk '{print $2}')
 			# size and nfsSize are treated as strings because ',' and 'T'
 		        if [ ! $ISSIZE = ${VALUES[$SIZE]} ]; then
-                		MES1="Warning size $ISSIZE is not equal to nfsSize ${VALUES[$SIZE]}"
+                		MES1="$ISSIZE ${VALUES[$SIZE]} $MESSAGE"
 		                logit
 		        fi
 		;;
 		$IS_ROOT)
+			MES="      $TITLE"
+			echoIt
 			whoami | grep 'root' > /dev/null
 			if [ ! $? -eq 0 ]; then
-				MES="$SCRIPT user needs to be 'root'"
-				loggerExit
-			fi
-		;;
-		$IS_MOUNTED)
-			df -h | grep ${VALUES[$MOUNT]} > /dev/null
-			if [ $? -eq 0 ]; then
-				MES="${VALUES[$MOUNT]} already mounted, use 'mount.sh unmount' first"
+				MES="$MESSAGE"
 				loggerExit
 			fi
 		;;
@@ -317,14 +411,13 @@ checkCommandLineParameter() {
 }
 
 # run before 'mount' program line value checks
-beforeChecks() {
-        MES="beforeChecks"
+checks1() {
+        MES="checks1"
         echoIt
-        for NAME in "${!BEFORE_CHECKS[@]}"
+        for TITLE in "${!CHECK1[@]}"
         do
-        	RETVAL=${BEFORE_RETVALS[$NAME]}
-		CHECK=${BEFORE_CHECKS[$NAME]}
-		VALUE=${VALUES[$NAME]}
+		MESSAGE=${CHECK1[$TITLE]}
+        	RETVAL=${ERROR1[$TITLE]}
                 checkCommandLineParameter
         done
         MES=""
@@ -332,14 +425,42 @@ beforeChecks() {
 }
 
 # run before 'mount' program line status checks
-middleChecks() {
-        MES="middleChecks"
+checks2() {
+        MES="checks2"
         echoIt
-        for NAME in "${!MIDDLE_CHECKS[@]}"
+        for TITLE in "${!CHECK2[@]}"
         do
-                RETVAL=${MIDDLE_RETVALS[$NAME]}
-                CHECK=${MIDDLE_CHECKS[$NAME]}
-                VALUE=${VALUES[$NAME]}
+		MESSAGE=${CHECK2[$TITLE]}
+                RETVAL=${ERROR2[$TITLE]}
+                checkCommandLineParameter
+        done
+        MES=""
+        echoIt
+}
+
+# run before 'mount' program line status checks
+checks3() {
+        MES="checks3"
+        echoIt
+        for TITLE in "${!CHECK3[@]}"
+        do
+		MESSAGE=${CHECK3[$TITLE]}
+                RETVAL=${ERROR3[$TITLE]}
+                checkCommandLineParameter
+        done
+        MES=""
+        echoIt
+}
+
+
+# run before command
+checks4() {
+        MES="checks4"
+        echoIt
+        for TITLE in "${!CHECK4[@]}"
+        do
+		MESSAGE=${CHECK4[$TITLE]}
+                RETVAL=${ERROR4[$TITLE]}
                 checkCommandLineParameter
         done
         MES=""
@@ -347,33 +468,41 @@ middleChecks() {
 }
 
 # run after command line value checks
-afterChecks() {
-        MES="afterChecks"
+checks5() {
+        MES="checks5"
         echoIt
-        for NAME in "${!AFTER_CHECKS[@]}"
+        for TITLE in "${!CHECK5[@]}"
         do
-                RETVAL=${AFTER_RETVALS[$NAME]}
-                CHECK=${AFTER_CHECKS[$NAME]}
-                VALUE=${VALUES[$NAME]}
+                MESSAGE=${CHECK5[$TITLE]}
+                RETVAL=${ERROR5[$TITLE]}
                 checkCommandLineParameter
         done
         MES=""
         echoIt
 }
 
-# checks before executing the 'umount' program
-checkMount() {
-        MES="mountChecks"
-        echoIt
-        for NAME in "${!MOUNT_CHECKS[@]}"
-        do
-                RETVAL=${MOUNT_RETVALS[$NAME]}
-                CHECK=${MOUNT_CHECKS[$NAME]}
-                VALUE=${VALUES[$NAME]}
-                checkCommandLineParameter
-        done
-        MES=""
-        echoIt
+usage() {
+        echo "mount.sh, mount given host:export upon given mountpoint"
+        echo "commands are:"
+        echo "  -->   mount, mount the export onto the mountpoint (default)"
+        echo "  --> unmount, unmount the mountpoint"
+        echo "  -->    free, show used and free space on given mountpoint"
+        echo "  -->    show, do nothing, show given parameter values"
+        echo "  --> silence, do not echo each action"
+        echo "  -->   usage, show this message"
+        echo ""
+        echo "parameters are:"
+        echo "  -->       tag=, the TAG to use for the 'logger' command, see /var/log/syslog"
+        echo "  -->   nfshost=, the host providing the NFS service"
+        echo "  --> nfsexport=, the exported directory"
+        echo "  -->  nfsmount=, the mountpoint to mount the export upon"
+        echo "  -->   nfsport=, optional, the port the NFS service is provided"
+        echo "  -->   nfssize=, optional, the expected size once the exported volume is mounted"
+        echo
+        echo "the script needs to run as 'root', be 'root' or use 'sudo'"
+        echo
+        echo "example:"
+        echo "sudo mount.sh silence mount tag='DATA' nfshost=ch3snas nfsexport=/mnt/HD_a2/data nfsmount=/data nfsport=2049 nfssize=1,8T"
 }
 
 # show all values
@@ -405,24 +534,26 @@ readCommandLineParameters
 if [ -z ${VALUES[$TNAME]} ]; then VALUES[$TNAME]="MOUNT"; fi
 if [ -z $COMMAND ];          then COMMAND='mount'; fi
 if [ -z ${VALUES[$PORT]} ];  then VALUES[$PORT]=2049; fi
-
-hasCommandLineParameters
+# problably add SERVICE at a later time
 
 case $COMMAND in
     mount)
-	beforeChecks
-	middleChecks
+	hasCommandLineParameters
+	checks1
+	checks2
+	checks3
 	mount ${VALUES[$HOST]}:${VALUES[$EXPORT]} ${VALUES[$MOUNT]}
 	if [ ! $? -eq 0 ]; then
 	        MES="Some error while mounting ${VALUES[$HOST]}:${VALUES[$EXPORT]} onto ${VALUES[$MOUNT]}"
         	loggerExit
 	fi
-	afterChecks
+	checks5
 	MES="${VALUES[$MOUNT]} mounted"
 	echoIt
     ;;
     unmount)
-	checkMount
+	hasCommandLineParameters
+	checks4
 	umount ${VALUES[$MOUNT]}
         if [ ! $? -eq 0 ]; then
                 MES="Some error while unmounting ${VALUES[$MOUNT]}"
@@ -435,37 +566,19 @@ case $COMMAND in
 	showValues
     ;;
     free)
-	checkMount
+	hasCommandLineParameters
+	checks4
 	df -h | grep ${VALUES[$MOUNT]}
     ;;
     exports)
+	hasCommandLineParameters
 	showmount -e ${VALUES[$HOST]}
     ;;
     usage)
-	echo "mount.sh, mount given host:export upon given mountpoint"
-	echo "commands are:"
-        echo "  -->   mount, mount the export onto the mountpoint (default)"
-        echo "  --> unmount, unmount the mountpoint"
-        echo "  -->    free, show used and free space on given mountpoint"
-	echo "  -->    show, do nothing, show given parameter values"
-        echo "  --> silence, do not echo each action"
-        echo "  -->   usage, show this message"
-        echo ""
-        echo "parameters are:"
-	echo "  -->       tag=, the TAG to use for the 'logger' command, see /var/log/syslog"
-	echo "  -->   nfshost=, the host providing the NFS service"
-        echo "  --> nfsexport=, the exported directory"
-        echo "  -->  nfsmount=, the mountpoint to mount the export upon"
-	echo "  -->   nfsport=, optional, the port the NFS service is provided"
-        echo "  -->   nfssize=, optional, the expected size once the exported volume is mounted"
-        echo
-        echo "the script needs to run as 'root', be 'root' or use 'sudo'"
-        echo
-        echo "example:"
-	echo "sudo mount.sh silence mount tag='DATA' nfshost=ch3snas nfsexport=/mnt/HD_a2/data nfsmount=/data nfsport=2049 nfssize=1,8T"
+	usage
     ;;
     *)  echo "$SCRIPT version $VERSION missing command and parameters"
-	echo "   use 'mount.sh usage' to learn more"
+	usage
     ;;
 esac
 
